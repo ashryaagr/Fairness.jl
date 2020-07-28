884890da7e8427f5f598644ae5d855f75cb607b7
ashryaagr <ashryaagr@gmail.com>
2020-07-28 12:27:06 +0530

    added nextjournal example

--- examples/compas_neuralnet_blog.jl
+++ examples/compas_neuralnet_blog.jl
@@ -1,40 +1,46 @@
 using Fairness
-using MLJ
-using PrettyPrinting
+using MLJBase, MLJModels
 
 data = @load_compas
 X, y = data
 
-@load NeuralNetworkClassifier
-model = @pipeline ContinuousEncoder NeuralNetworkClassifier
+@load RandomForestClassifier pkg=DecisionTree
+model = @pipeline ContinuousEncoder RandomForestClassifier
 
 wrappedModel = ReweighingSamplingWrapper(classifier=model, grp=:race)
 wrappedModel2 = LinProgWrapper(classifier=wrappedModel, grp=:race,
                                 measure=false_positive_rate)
 
 result = evaluate(wrappedModel2,
-  X, y,
-  measures=MetricWrappers(
-    [true_positive_rate, false_positive], grp=:race)) |> pprint
+    X, y,
+    measures=[
+    Disparity(false_positive_rate, refGrp="Caucasian", grp=:race),
+    MetricWrapper(accuracy, grp=:race)])
 
+result_1 = evaluate(model,
+    X, y,
+    measures=[
+    Disparity(false_positive_rate, refGrp="Caucasian", grp=:race),
+    MetricWrapper(accuracy, grp=:race)])
 
-train, test = partition(eachindex(y), 0.7, shuffle=true)
-mach = machine(wrappedModel2, X, y)
-fit!(mach, rows=train)
+using VegaLite
+using DataFrames
+n_grps = length(levels(X[!, :race]))
+dispVals = collect(values(result.measurement[1]))
+dispVals_1 = collect(values(result_1.measurement[1]))
+df = DataFrame(disparity=vcat(dispVals, dispVals_1),
+  algo=vcat(repeat(["Wrapped Model"], n_grps+1), repeat(["ML Model"], n_grps+1)),
+  grp=repeat(collect(keys(result.measurement[1])), 2))
 
-
-ŷ = predict(mach, rows=test)
-ŷ |> pprint
-
-
-ft = fair_tensor(ŷ, y[test], X[test, :gender_status])
-
-df_disparity = disparity(
-  [true_positive_rate, positive_predictive_value],
-  ft, refGrp="male_single",
-  func=(x, y)->(x-y)/y
-)
-
-parity(df_disparity,
-  func= (x) -> abs(x)<0.07
+df |> @vlplot(
+  :bar,
+  column="grp:o",
+  y={"disparity:q",axis={title="False Positive Rate Disparity"}},
+  x={"algo:o", axis={title=""}},
+  color={"algo:o"},
+  spacing=20,
+  config={
+  view={stroke=:transparent},
+  axis={domainWidth=1}
+  }
 )
