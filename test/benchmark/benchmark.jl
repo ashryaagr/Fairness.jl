a2e0b5fe42f11a88a9bf22ee7cfa51f59f0bab2f
ashryaagr <ashryaagr@gmail.com>
2020-09-09 20:02:31 +0530

    version update and Updated tests for benchmark function

--- test/benchmark/benchmark.jl
+++ test/benchmark/benchmark.jl
@@ -2,7 +2,10 @@
 	model = ConstantClassifier()
 	X, y = @load_toydata
 	results = fairevaluate([model, model], X, y, grp=:Sex, priv_grps=["M"])
-	@test length(results)==2
-	@test size(results[1])==(2, 2)
-	@test size(results[2])==(2, 2)
+	@test length(results)==5
+	@test size(results["measures"])==(1,)
+	@test size(results["pvalues"])==(1, 2, 2)
+	@test size(results["classifier_names"])==(2,)
+	@test size(results["results"])==(1, 2, 6)
+	@test size(results["tstats"])==(1, 2, 2)
 end
