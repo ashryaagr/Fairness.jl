50f974d67e1e7c677733f39954e14855aee2c200
ashryaagr <ashryaagr@gmail.com>
2020-09-16 20:26:50 +0530

    disparate impact

--- src/benchmark/utils.jl
+++ src/benchmark/utils.jl
@@ -3,7 +3,7 @@ function disparate_impact(ft::Fairness.FairTensor; priv_grps::Array{String, 1})
 	priv_count, unpriv_count = 0, 0
 	for grp in ft.labels
 		if grp in priv_grps
-			den += sum(ft[Fairness._ftIdx(ft, grp), 2, :])
+			den += sum(ft[Fairness._ftIdx(ft, grp), 1, :])
 			priv_count += sum(ft[Fairness._ftIdx(ft, grp), :, :])
 		else
 			num += sum(ft[Fairness._ftIdx(ft, grp), 1, :])
@@ -18,6 +18,10 @@ struct DisparateImpact <: Fairness.Measure
 	priv_grps::Array{String, 1}
 end
 
+function DisparateImpact(;grp, priv_grps)
+	return DisparateImpact(grp, priv_grps)
+end
+
 function (di::DisparateImpact)(ŷ, X, y)
 	if typeof(ŷ[1]) <: MLJBase.UnivariateFinite
 		ŷ = StatsBase.mode.(ŷ)
@@ -25,6 +29,19 @@ function (di::DisparateImpact)(ŷ, X, y)
 	grps = X[:, di.grp]
 	ft = fair_tensor(categorical(ŷ), categorical(y), categorical(grps))
 	return disparate_impact(ft, priv_grps=di.priv_grps)
+end
+
+function (di::DisparateImpact)(ft::FairTensor; grp=:)
+	num, den = 0, 0
+	priv_count, unpriv_count = 0, 0
+	priv_grps = di.priv_grps
+	for grp in priv_grps
+		den += sum(_calcmetric(ft, grp, 1, :))
+		priv_count += sum(ft[Fairness._ftIdx(ft, grp), :, :])
+	end
+	num += sum(_calcmetric(ft, grp, 1, :))
+	unpriv_count += sum(_calcmetric(ft, grp, :, :))
+	return (num/unpriv_count)/(den/priv_count)
 end
 
 MLJBase.name(::Type{<:DisparateImpact}) = "DisparateImpact"
