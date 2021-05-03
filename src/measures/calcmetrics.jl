4b63eeedff4ab390690a7a5f1ea5611437300687
Sumantrak Mukherjee <sumantrak.mukherjee@gmail.com>
2021-05-03 20:27:51 +0530

    Syntax errors solved

--- src/measures/calcmetrics.jl
+++ src/measures/calcmetrics.jl
@@ -54,62 +54,62 @@ struct TruePositiveRateDifference <: Measure end
 TPRD = TruePositiveRateDifference
 true_positive_rate_difference = TPRD()
 tprd = true_positive_rate_difference
-(::TPRD)(ft::FairTensor; grp1=:,grp2=:)=abs(true_positive_rate(ft;grp=grp1)-true_positive_rate(ft;grp=grp2))
+(::TPRD)(ft::FairTensor; grp1=:;grp2=:)=abs(true_positive_rate(ft;grp=grp1)-true_positive_rate(ft;grp=grp2))
 
 struct FalsePositiveRateDifference <: Measure end
 FPRD = FalsePositiveRateDifference
 false_positive_rate_difference = FPRD()
 fprd = false_positive_rate_difference
-(::FPRD)(ft::FairTensor; grp1=:,grp2=:)=abs(false_positive_rate(ft;grp=grp1)-false_positive_rate(ft;grp=grp2))
+(::FPRD)(ft::FairTensor; grp1=:;grp2=:)=abs(false_positive_rate(ft;grp=grp1)-false_positive_rate(ft;grp=grp2))
 
 struct FalseNegativeRateDifference <: Measure end
 FNRD = FalseNegativeRateDifference
 false_negative_rate_difference = FNRD()
 fnrd = false_negative_rate_difference
-(::FNRD)(ft:FairTensor; grp1=:, grp2=:)=abs(false_negative_rate(ft;grp=grp1)-false_negative_rate(ft;grp=grp2))
+(::FNRD)(ft::FairTensor; grp1=:, grp2=:)=abs(false_negative_rate(ft;grp=grp1)-false_negative_rate(ft;grp=grp2))
 
 struct FalseOmissionRateDifference <: Measure end
 FORD = FalseOmissionRateDifference
 false_omission_rate_difference = FORD()
 ford = false_omission_rate_difference
-(::FORD)(ft:FairTensor; grp1=:, grp2=:)=abs(false_omission_rate(ft;grp=grp1)-false_omission_rate(ft;grp=grp2))
+(::FORD)(ft::FairTensor; grp1=:, grp2=:)=abs(false_omission_rate(ft;grp=grp1)-false_omission_rate(ft;grp=grp2))
 
 struct FalseDiscoveryRateDifference <: Measure end
 FDRD = FalseDiscoveryRateDifference
 false_discovery_rate_difference = FDRD()
 fdrd = false_discovery_rate_difference
-(::FDRD)(ft:FairTensor; grp1=:, grp2=:)=abs(false_discovery_rate(ft;grp=grp1)-false_discovery_rate(ft;grp=grp2))
+(::FDRD)(ft::FairTensor; grp1=:, grp2=:)=abs(false_discovery_rate(ft;grp=grp1)-false_discovery_rate(ft;grp=grp2))
 
 struct FalsePositiveRateRatio <: Measure end
 FPRR = FalsePositiveRateRatio
 false_positive_rate_ratio = FPRR()
 fprr = false_positive_rate_ratio
-(::FPRR)(ft:FairTensor; gpr1=:, grp2=:)=false_positve_rate(ft;grp=grp1)/false_positive_rate(ft;grp=grp2)
+(::FPRR)(ft::FairTensor; gpr1=:, grp2=:)=false_positve_rate(ft;grp=grp1)/false_positive_rate(ft;grp=grp2)
 
 struct FalseNegativeRateRatio <: Measure end
 FNRR = FalseNegativeRateRatio
 false_negative_rate_ratio = FNRR()
 fnrr = false_negative_rate_ratio
-(::FNRR)(ft:FairTensor; gpr1=:, grp2=:)=false_negative_rate(ft;grp=grp1)/false_negative_rate(ft;grp=grp2)
+(::FNRR)(ft::FairTensor; gpr1=:, grp2=:)=false_negative_rate(ft;grp=grp1)/false_negative_rate(ft;grp=grp2)
 
 
 struct FalseOmissionRateRatio <: Measure end
 FORR = FalseOmissionRateRatio
 false_omission_rate_ratio = FORR()
 forr = false_omission_rate_ratio
-(::FORR)(ft:FairTensor; grp1=:, grp2=:)=false_omission_rateft;grp=grp1)/false_omission_rate(ft;grp=grp2)
+(::FORR)(ft::FairTensor; grp1=:, grp2=:)=false_omission_rateft;grp=grp1)/false_omission_rate(ft;grp=grp2)
 
 struct FalseDiscoveryRateRatio <: Measure end
 FDRR = FalseDiscoveryRateRatio
 false_discovery_rate_ratio = FDRR()
 fdrr = false_discovery_rate_ratio
-(::FDRR)(ft:FairTensor; grp1=:, grp2=:)=false_discovery_rate(ft;grp=grp1)/false_discovery_rate(ft;grp=grp2)
+(::FDRR)(ft::FairTensor; grp1=:, grp2=:)=false_discovery_rate(ft;grp=grp1)/false_discovery_rate(ft;grp=grp2)
 
 struct AverageOddsDifference <: Measure end
 AOD = AverageOddsDifference
 average_odds_difference = AOD()
 aod = average_odds_difference
-(::AOD)(ft:FairTensor; grp1=:, grp2=:)=0.5*(false_positive_rate_difference(ft;grp1=grp1;grp2=grp2)+true_positive_rate_difference(ft;grp1=grp1,grp2=grp2))
+(::AOD)(ft::FairTensor; grp1=:; grp2=:)=0.5*(false_positive_rate_difference(ft;grp1=grp1;grp2=grp2)+true_positive_rate_difference(ft;grp1=grp1,grp2=grp2))
 
 
 
